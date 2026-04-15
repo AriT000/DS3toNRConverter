a783ba93886333beb5eaed0a54cba5848294e4e9
Ari Torczon <113228606+AriT000@users.noreply.github.com>
2026-04-14 22:50:16 -0700

    Add files via upload

--- ConvertAll.bat
+++ ConvertAll.bat
@@ -10,8 +10,7 @@ set "OUTPUT=%ROOT%\output"
 
 if not exist "%OUTPUT%" mkdir "%OUTPUT%"
 
-call :run_atk "AtkParam_Npc" "DS3toNR_ATKConverter.py" "AtkParam_Npc.csv" "AtkTemplate.csv"
-
+call :run_std "AtkParam_Npc"     "DS3toNR_ATKConverter.py"       "AtkParam_Npc.csv"     "AtkTemplate.csv"
 call :run_std "BehaviorParam"    "DS3toNR_BehaviorConvert.py"    "BehaviorParam.csv"    "BehaviorTemplate.csv"
 call :run_std "Bullet"           "DS3toNR_BulletConvert.py"      "Bullet.csv"           "BulletTemplate.csv"
 call :run_std "LockCamParam"     "DS3toNR_LockCamConvert.py"     "LockCamParam.csv"     "LockCamTemplate.csv"
@@ -24,34 +23,6 @@ call :run_std "ThrowParam"       "DS3toNR_ThrowConvert.py"       "ThrowParam.csv
 echo Done. Output files are in "%OUTPUT%"
 pause
 exit /b 0
-
-:run_atk
-set "FOLDER=%~1"
-set "SCRIPT=%CONVERTERS%\%~1\%~2"
-set "INFILE=%INPUT%\%~3"
-set "OUTFILE=%OUTPUT%\%~3"
-set "TEMPLATE=%CONVERTERS%\%~1\%~4"
-
-if not exist "%SCRIPT%" (
-    echo [skip] %~1
-    goto :eof
-)
-if not exist "%INFILE%" (
-    echo [skip] %~1
-    goto :eof
-)
-if not exist "%TEMPLATE%" (
-    echo [skip] %~1
-    goto :eof
-)
-
-echo [run] %~1
-python "%SCRIPT%" "%INFILE%" "%OUTFILE%" --template "%TEMPLATE%"
-if errorlevel 1 (
-    echo [fail] %~1
-    exit /b 1
-)
-goto :eof
 
 :run_std
 set "FOLDER=%~1"
